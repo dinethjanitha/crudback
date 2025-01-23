5846840e17a2ac7170464ace47be821684222978
dinethjanitha <djanithak@gmail.com>
2025-01-23 17:42:05 +0530

    initialize spring projrct

---
+++ src/main/java/com/example/crudback/CrudbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudbackApplication.class, args);
+    }
+
+}
